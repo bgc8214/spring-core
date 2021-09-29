03a0bee23af8a29c2ac29722a40b2ff33e6107eb
boss.back <boss.back@kakaocorp.com>
2021-09-29 20:42:15 +0900

    feat: 회원도메인 테스트

---
+++ src/main/java/core/order/MemberApp.java
@@ -0,0 +1,18 @@
+package core.order;
+
+import core.order.member.*;
+import core.order.member.MemberService;
+import core.order.member.MemberServiceImpl;
+
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
