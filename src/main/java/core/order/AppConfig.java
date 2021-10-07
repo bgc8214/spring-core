260ef602b48adf0984bb457aa037663c631b373a
boss.back <boss.back@kakaocorp.com>
2021-10-07 13:04:18 +0900

    fix: AppConfig 리팩토링

--- src/main/java/core/order/AppConfig.java
+++ src/main/java/core/order/AppConfig.java
@@ -1,6 +1,8 @@
 package core.order;
 
+import core.order.discount.DiscountPolicy;
 import core.order.discount.FixDiscountPolicy;
+import core.order.member.MemberRepository;
 import core.order.member.MemberService;
 import core.order.member.MemberServiceImpl;
 import core.order.member.MemoryMemberRepository;
@@ -10,11 +12,19 @@ import core.order.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 
 
